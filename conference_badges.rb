4bae755f38b7c80473d2eb218e7e30946aa829cd
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-28 23:21:59 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  newArr=[]
+  arr.each do |name|
+     newArr.push("Hello, my name is #{name}.")
+  end
+  return newArr
+end
+
+def assign_rooms(names)
+  count=1
+  newArr=[]
+  names.each do |name|
+  newArr<< "Hello, #{name}! You'll be assigned to room #{count}!"
+  count+=1 
+  end
+  return newArr
+end
+
+def printer(str)
+  batch_badge_creator(str).each do |each|
+    puts each
+  end
+  assign_rooms(str).each do |each|
+   puts each
+  end
+end
