9ebe41f03dc0645d4fe85cbe41e8fe0f34ecd5d0
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-28 22:46:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,11 @@ def batch_badge_creator(arr)
   arr.each do |name|
     puts "Hello,my name is #{name}."
 end
+
+def assign_rooms(names)
+  count=1
+  names.each do |name|
+  puts "Hello,#{name}!You'll be assigned to room #{count}!"
+  count+=1 
+end
+end
