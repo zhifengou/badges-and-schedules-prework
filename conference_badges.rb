4022200c42f86c58231d64650a1118430ae47c14
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-28 22:42:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,9 @@
 # Write your code here.
 def badge_maker(name)
   puts "Hello,my name is #{name}."
+end
 
 def batch_badge_creator(arr)
-  arr.each()
+  arr.each do |name|
+    puts "Hello,my name is #{name}."
+end
